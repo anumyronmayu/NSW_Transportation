fbef63dbfea15c11ae140d29a807653f6aac1242
Yu Ma <myronmayu@gmail.com>
2015-12-14 15:55:28 +1100

    Routes and Stops

--- src/analysing_CSV_Datasets/Stop_Times/Extract_Stop_Times_Data2.java
+++ src/analysing_CSV_Datasets/Stop_Times/Extract_Stop_Times_Data2.java
@@ -19,22 +19,104 @@ import java.util.Set;
 
 import analysing_CSV_Datasets.Routes.*;
 
-/*
- * Version 2
- */
 public class Extract_Stop_Times_Data2 {
 
 	private static void makeDir(String dir) {
 		new File(dir).mkdirs();
 	}
 
+	private void analysingATypeOfNetwork(String folderName,
+			HashMap<String, ArrayList<Stop_Times>> map, List<String> typeList,
+			String type) throws IOException {
+
+		List<ArrayList<Double>> velocitiesListSydneyBusesNetwork = new ArrayList<ArrayList<Double>>();
+		List<ArrayList<String>> routeAndBusNumberListSydneyBusesNetwork = new ArrayList<ArrayList<String>>();
+
+		for (String s : typeList) {
+			ArrayList<Stop_Times> stop_times_group = map.get(s);
+			ArrayList<Double> velocities = new ArrayList<Double>();
+
+			for (int i = 1; i < stop_times_group.size(); i++) {
+				double time = ((double) (stop_times_group.get(i)
+						.getArrival_time().getTime() - stop_times_group
+						.get(i - 1).getDeparture_time().getTime())) / 1000 / 60 / 60;// hour
+				if (time == 0) {
+					continue;
+				}
+				double d = (stop_times_group.get(i).getShape_dist_traveled() - stop_times_group
+						.get(i - 1).getShape_dist_traveled()) / 1000;
+				double v = d / time;
+				velocities.add(v);
+			}
+			velocitiesListSydneyBusesNetwork.add(velocities);
+
+			ArrayList<String> routeAndBusNumber = new ArrayList<String>();
+			String[] splitStr = stop_times_group.get(0).getTrip_id().split("-");
+			String[] splitStr1 = splitStr[0].split("\\.");
+			routeAndBusNumber.add(splitStr1[2]);
+			if (splitStr.length == 4) {
+				routeAndBusNumber.add(splitStr[1]);
+			} else {
+				routeAndBusNumber.add(splitStr[1] + "-" + splitStr[2]);
+			}
+			routeAndBusNumberListSydneyBusesNetwork.add(routeAndBusNumber);
+		}
+
+		List<Double> averageVelocityList = new ArrayList<Double>();
+		for (ArrayList<Double> velocitiesSequence : velocitiesListSydneyBusesNetwork) {
+			double sumVelocity = 0;
+			for (double velocity : velocitiesSequence) {
+				sumVelocity += velocity;
+			}
+			averageVelocityList.add(sumVelocity / velocitiesSequence.size());
+		}
+
+		DecimalFormat df = new DecimalFormat("#.##");
+
+		List<String> results = new ArrayList<String>();
+
+		makeDir(folderName + "Analysis_Results/Stop_Times");
+
+		FileWriter writer = new FileWriter(folderName
+				+ "Analysis_Results/Stop_Times/Stop_Times_Analysis_Results_"
+				+ type + ".txt");
+		FileWriter writerCSV = new FileWriter(folderName
+				+ "Analysis_Results/Stop_Times/Stop_Times_Analysis_Results_"
+				+ type + "_csv.csv");
+		writerCSV.write("\"Route\",\"BusNumber\",\"AverageVelocity\"\n");
+		double sum = 0;
+		for (int i = 0; i < averageVelocityList.size(); i++) {
+			double v = averageVelocityList.get(i);
+			sum += v;
+			String route = routeAndBusNumberListSydneyBusesNetwork.get(i)
+					.get(0);
+			String busNumber = routeAndBusNumberListSydneyBusesNetwork.get(i)
+					.get(1);
+			String result = ("Route: " + route + " Bus Number: " + busNumber
+					+ " Average Velocity: " + df.format(v) + "km/h");
+			results.add(result);
+			writer.write(result + "\n");
+			writerCSV.write("\"" + route + "\",\"" + busNumber + "\",\""
+					+ df.format(v) + "\"\n");
+		}
+		writer.close();
+		writerCSV.close();
+		double average = sum / averageVelocityList.size();
+
+		System.out.println("Type: " + type + "Average Velocity: " + df.format(average) + "km/h");
+
+	}
+
 	public static void main(String[] args) throws IOException, ParseException {
+
+		Extract_Stop_Times_Data2 obj = new Extract_Stop_Times_Data2();
 
 		// route_id => type
 		Extract_Routes_Data routeData = new Extract_Routes_Data();
 		HashMap<String, String> typeMapWithVersionNumber = routeData
 				.getTypeMapWithVersionNumber();
 
+		// read csv file
 		String folderName = "/Users/Myron/Documents/2015_nswtransport/GTFS/full_greater_sydney_gtfs_static_csv/";
 		String csv_file = folderName
 				+ "Analysis_Results/stop_times_modified.csv";
@@ -43,11 +125,9 @@ public class Extract_Stop_Times_Data2 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(in));
 		String strLine = br.readLine();
 
-		List<Stop_Times> list = new ArrayList<Stop_Times>();
-		HashMap<String, ArrayList<Stop_Times>> map = new HashMap<String, ArrayList<Stop_Times>>();
-
+		// strLine => obj
 		int index = 0;
-
+		List<Stop_Times> list = new ArrayList<Stop_Times>();
 		while ((strLine = br.readLine()) != null) {
 			index++;
 
@@ -84,12 +164,8 @@ public class Extract_Stop_Times_Data2 {
 
 		br.close();
 
-		// List<ArrayList<Double>> velocitiesList = new
-		// ArrayList<ArrayList<Double>>();
-		// List<ArrayList<String>> routeAndBusNumberList = new
-		// ArrayList<ArrayList<String>>();
-
 		// trip_id => List<Stop_Times>
+		HashMap<String, ArrayList<Stop_Times>> map = new HashMap<String, ArrayList<Stop_Times>>();
 		for (int i = 0; i < list.size(); i++) {
 			Stop_Times st = list.get(i);
 			if (map.get(st.getTrip_id()) == null) {
@@ -125,10 +201,9 @@ public class Extract_Stop_Times_Data2 {
 				typeMap.put(entry.getKey(),
 						typeMapWithVersionNumber.get(route_id));
 			}
-
 		}
 
-		// type list
+		// type, list of trip_id
 		List<String> listOfSydneyBusesNetwork = new ArrayList<String>();
 		List<String> listOfRegionalTrainsandCoachesNetwork = new ArrayList<String>();
 		List<String> listOfPrivatebusservices = new ArrayList<String>();
@@ -194,107 +269,37 @@ public class Extract_Stop_Times_Data2 {
 			}
 		}
 
-		List<ArrayList<Double>> velocitiesListSydneyBusesNetwork = new ArrayList<ArrayList<Double>>();
-		List<ArrayList<String>> routeAndBusNumberListSydneyBusesNetwork = new ArrayList<ArrayList<String>>();
-
-		for (String s : listOfSydneyBusesNetwork) {
-			ArrayList<Stop_Times> stop_times_group = map.get(s);
-			ArrayList<Double> velocities = new ArrayList<Double>();
-
-			for (int i = 1; i < stop_times_group.size(); i++) {
-				double time = ((double) (stop_times_group.get(i)
-						.getArrival_time().getTime() - stop_times_group
-						.get(i - 1).getDeparture_time().getTime())) / 1000 / 60 / 60;// hour
-				if (time == 0) {
-					continue;
-				}
-				double d = (stop_times_group.get(i).getShape_dist_traveled() - stop_times_group
-						.get(i - 1).getShape_dist_traveled()) / 1000;
-				double v = d / time;
-				velocities.add(v);
-			}
-
-			velocitiesListSydneyBusesNetwork.add(velocities);
-
-			ArrayList<String> routeAndBusNumber = new ArrayList<String>();
-			String[] splitStr = stop_times_group.get(0).getTrip_id().split("-");
-			String[] splitStr1 = splitStr[0].split("\\.");
-			routeAndBusNumber.add(splitStr1[2]);
-			if (splitStr.length == 4) {
-				routeAndBusNumber.add(splitStr[1]);
-			} else {
-				routeAndBusNumber.add(splitStr[1] + "-" + splitStr[2]);
-			}
-			routeAndBusNumberListSydneyBusesNetwork.add(routeAndBusNumber);
-		}
-
-		/*
-		 * Set<Map.Entry<String, ArrayList<Stop_Times>>> entrySet =
-		 * map.entrySet(); for (Map.Entry<String, ArrayList<Stop_Times>> entry :
-		 * entrySet) {
-		 * 
-		 * ArrayList<Stop_Times> stop_times_group = entry.getValue();
-		 * ArrayList<Double> velocities = new ArrayList<Double>();
-		 * 
-		 * for (int i = 1; i < stop_times_group.size(); i++) { double time =
-		 * ((double) (stop_times_group.get(i) .getArrival_time().getTime() -
-		 * stop_times_group .get(i - 1).getDeparture_time().getTime())) / 1000 /
-		 * 60 / 60;// hour if (time == 0) { continue; } double d =
-		 * (stop_times_group.get(i).getShape_dist_traveled() - stop_times_group
-		 * .get(i - 1).getShape_dist_traveled()) / 1000; double v = d / time;
-		 * velocities.add(v); }
-		 * 
-		 * velocitiesList.add(velocities);
-		 * 
-		 * ArrayList<String> routeAndBusNumber = new ArrayList<String>();
-		 * String[] splitStr = stop_times_group.get(0).getTrip_id().split("-");
-		 * String[] splitStr1 = splitStr[0].split("\\.");
-		 * routeAndBusNumber.add(splitStr1[2]); if (splitStr.length == 4) {
-		 * routeAndBusNumber.add(splitStr[1]); } else {
-		 * routeAndBusNumber.add(splitStr[1] + "-" + splitStr[2]); }
-		 * routeAndBusNumberList.add(routeAndBusNumber);
-		 * 
-		 * }
-		 */
-
-		List<Double> averageVelocityList = new ArrayList<Double>();
-
-		for (ArrayList<Double> velocitiesSequence : velocitiesListSydneyBusesNetwork) {
-			double sumVelocity = 0;
-			for (double velocity : velocitiesSequence) {
-				sumVelocity += velocity;
-			}
-			averageVelocityList.add(sumVelocity / velocitiesSequence.size());
-		}
-
-		DecimalFormat df = new DecimalFormat("#.##");
-
-		List<String> results = new ArrayList<String>();
-
-		makeDir(folderName + "Analysis_Results/Stop_Times");
-
-		FileWriter writer = new FileWriter(folderName
-				+ "Analysis_Results/Stop_Times/Stop_Times_Analysis_Results.txt");
-		FileWriter writerCSV = new FileWriter(
-				folderName
-						+ "Analysis_Results/Stop_Times/Stop_Times_Analysis_Results_csv.csv");
-		writerCSV.write("\"Route\",\"BusNumber\",\"AverageVelocity\"\n");
-		for (int i = 0; i < averageVelocityList.size(); i++) {
-			double v = averageVelocityList.get(i);
-			String route = routeAndBusNumberListSydneyBusesNetwork.get(i)
-					.get(0);
-			String busNumber = routeAndBusNumberListSydneyBusesNetwork.get(i)
-					.get(1);
-			String result = ("Route: " + route + " Bus Number: " + busNumber
-					+ " Average Velocity: " + df.format(v) + "km/h");
-			results.add(result);
-			writer.write(result + "\n");
-			writerCSV.write("\"" + route + "\",\"" + busNumber + "\",\""
-					+ df.format(v) + "\"\n");
-			// System.out.println(result);
-		}
-		writer.close();
-		writerCSV.close();
+		// Analyzing SydneyBusesNetwork
+		obj.analysingATypeOfNetwork(folderName, map, listOfSydneyBusesNetwork,
+				"SydneyBusesNetwork");
+		obj.analysingATypeOfNetwork(folderName, map,
+				listOfRegionalTrainsandCoachesNetwork,
+				"RegionalTrainsAndCoachesNetwork");
+		obj.analysingATypeOfNetwork(folderName, map, listOfPrivatebusservices,
+				"PrivateBusServices");
+		obj.analysingATypeOfNetwork(folderName, map,
+				listOfIllawarraBusesNetwork, "IllawarraBusesNetwork");
+		obj.analysingATypeOfNetwork(folderName, map,
+				listOfBlueMountainsBusesNetwork, "BlueMountainsBusesNetwork");
+		obj.analysingATypeOfNetwork(folderName, map,
+				listOfCentralCoastBusesNetwork, "CentralCoastBusesNetwork");
+		obj.analysingATypeOfNetwork(folderName, map, listOfTemporarybuses,
+				"Temporarybuses");
+		obj.analysingATypeOfNetwork(folderName, map, listOfHunterBusesNetwork,
+				"HunterBusesNetwork");
+		obj.analysingATypeOfNetwork(folderName, map, listOfSydneyTrainsNetwork,
+				"SydneyTrainsNetwork");
+		obj.analysingATypeOfNetwork(folderName, map,
+				listOfSydneyFerriesNetwork, "SydneyFerriesNetwork");
+		obj.analysingATypeOfNetwork(folderName, map,
+				listOfPrivateferryandfastferryservices,
+				"PrivateFerryAndFastFerryServices");
+		obj.analysingATypeOfNetwork(folderName, map,
+				listOfSydneyLightRailNetwork, "SydneyLightRailNetwork");
+		obj.analysingATypeOfNetwork(folderName, map,
+				listOfIntercityTrainsNetwork, "IntercityTrainsNetwork");
+		obj.analysingATypeOfNetwork(folderName, map, listOfNewcastleFerries,
+				"NewCastleFerries");
 
 	}
 }
