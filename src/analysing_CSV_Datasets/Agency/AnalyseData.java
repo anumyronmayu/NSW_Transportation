5f4929ad6b0cc8832370232482879c01ec91fbc0
Yu Ma <myronmayu@gmail.com>
2015-12-24 17:15:28 +0800

    Agency

--- src/analysing_CSV_Datasets/Agency/AnalyseData.java
+++ src/analysing_CSV_Datasets/Agency/AnalyseData.java
@@ -1,5 +1,31 @@
 package analysing_CSV_Datasets.Agency;
 
-public class AnalyseData {
+import java.util.ArrayList;
+import java.util.List;
 
+public class AnalyseData {
+	public List<Agency> parseStrLines(List<String> list) {
+
+		List<Agency> agencyList = new ArrayList<Agency>();
+
+		for (String s : list) {
+
+			String[] splitStr = s.split("\",\"");
+
+			Agency agency = new Agency();
+
+			agency.setId(splitStr[0]);
+			agency.setName(splitStr[1]);
+			agency.setUrl(splitStr[2]);
+			agency.setTimezone(splitStr[3]);
+			agency.setLang(splitStr[4]);
+			agency.setPhone(splitStr[5]);
+
+			agencyList.add(agency);
+
+		}
+
+		return agencyList;
+
+	}
 }
