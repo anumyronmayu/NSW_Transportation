e1973c4afba12041d8b7acbf93bda40a388b9703
Yu Ma <myronmayu@gmail.com>
2015-12-24 17:45:03 +0800

    Agency

---
+++ src/analysing_CSV_Datasets/Routes/AnalyseData.java
@@ -0,0 +1,13 @@
+package analysing_CSV_Datasets.Routes;
+
+import java.util.List;
+
+public class AnalyseData {
+
+	public List<Routes> parseStrLines(List<String> list) {
+		
+		return null;
+
+	}
+
+}
