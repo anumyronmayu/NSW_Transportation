3c44cb36809736147055b867253a5e74df69a685
Yu Ma <myronmayu@gmail.com>
2016-01-15 00:17:55 +1100

    CSV to TXT

--- src/utilities/Utilities.java
+++ src/utilities/Utilities.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Utilities {
-	
+
 	public static void makeDir(String dir) {
 		new File(dir).mkdirs();
 	}
@@ -37,6 +37,74 @@ public class Utilities {
 		br.close();
 
 		return list;
+
+	}
+
+	private static boolean isEmpty(int[] n) {
+
+		return n == null || n.length == 0;
+
+	}
+
+	public static void quickSort(int[] n) {
+
+		if (isEmpty(n))
+
+			return;
+
+		quickSort(n, 0, n.length - 1);
+
+	}
+
+	private static void quickSort(int[] n, int l, int h) {
+
+		if (isEmpty(n))
+
+			return;
+
+		if (l < h) {
+
+			int pivot = partion(n, l, h);
+
+			quickSort(n, l, pivot - 1);
+
+			quickSort(n, pivot + 1, h);
+
+		}
+
+	}
+
+	private static int partion(int[] n, int start, int end) {
+
+		int tmp = n[start];
+
+		while (start < end) {
+
+			while (n[end] >= tmp && start < end)
+
+				end--;
+
+			if (start < end) {
+
+				n[start++] = n[end];
+
+			}
+
+			while (n[start] < tmp && start < end)
+
+				start++;
+
+			if (start < end) {
+
+				n[end--] = n[start];
+
+			}
+
+		}
+
+		n[start] = tmp;
+
+		return start;
 
 	}
 
