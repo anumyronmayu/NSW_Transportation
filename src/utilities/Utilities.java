78e771b0c46f47ceca97112783d44daefc256b07
Yu Ma <myronmayu@gmail.com>
2016-02-05 16:12:56 +1100

    CSV to TXT

--- src/utilities/Utilities.java
+++ src/utilities/Utilities.java
@@ -1,15 +1,38 @@
 package utilities;
 
+import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Utilities {
+
+	public static int countLines(String filename) throws IOException {
+		InputStream is = new BufferedInputStream(new FileInputStream(filename));
+		try {
+			byte[] c = new byte[1024];
+			int count = 0;
+			int readChars = 0;
+			boolean empty = true;
+			while ((readChars = is.read(c)) != -1) {
+				empty = false;
+				for (int i = 0; i < readChars; ++i) {
+					if (c[i] == '\n') {
+						++count;
+					}
+				}
+			}
+			return (count == 0 && !empty) ? 1 : count;
+		} finally {
+			is.close();
+		}
+	}
 
 	public static void makeDir(String dir) {
 		new File(dir).mkdirs();
@@ -108,4 +131,19 @@ public class Utilities {
 
 	}
 
+	public static void main(String... args) {
+		File[] files = new File("C:/").listFiles();
+		showFiles(files);
+	}
+
+	public static void showFiles(File[] files) {
+		for (File file : files) {
+			if (file.isDirectory()) {
+				System.out.println("Directory: " + file.getName());
+				showFiles(file.listFiles()); // Calls same method again.
+			} else {
+				System.out.println("File: " + file.getName());
+			}
+		}
+	}
 }
